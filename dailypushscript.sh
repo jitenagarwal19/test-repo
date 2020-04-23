12acad81d057b37b3149f168238514b6f6050d8e
Jiten agarwal <jitenagarwal19@gmail.com>
2020-04-23 11:23:49 +0530

    script commit 04-23-2020

--- dailypushscript.sh
+++ dailypushscript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export current_date=$(date +%m-%d-%Y)
-commit_message="script commit"
+commit_message="script commit ${current_date}"
 git add .
 echo "will commit file now"
 git commit -m "${commit_message}"
